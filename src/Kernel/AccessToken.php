87bfc173777689694e1c3ec3eb6ec44211819db9
Nick <i@httpd.cc>
2022-03-01 10:53:01 +0800

    perf

---
+++ src/Kernel/AccessToken.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cblink\Service\Order\Kernel;
+
+class AccessToken extends \Cblink\Service\Foundation\AccessToken
+{
+
+    public function getBaseUrl()
+    {
+        return $this->app->config['idaas_url'];
+    }
+
+}
