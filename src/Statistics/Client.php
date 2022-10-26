1e5e50fa1ac0e0014240ff390b51fd08b32772ce
Nick <i@httpd.cc>
2022-10-26 14:13:05 +0800

    add statistics api

--- src/Statistics/Client.php
+++ src/Statistics/Client.php
@@ -19,6 +19,18 @@ class Client extends BaseApi
     }
 
     /**
+     * 订单金额统计
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function orderFee(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/amount', $query);
+    }
+
+    /**
      * 退单数量统计
      *
      * @param array $query
@@ -30,4 +42,15 @@ class Client extends BaseApi
         return $this->httpGet('/statistics/refund', $query);
     }
 
+    /**
+     * 退单金额统计
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function refundFee(array $query = [])
+    {
+        return $this->httpGet('/statistics/refund/amount', $query);
+    }
 }
