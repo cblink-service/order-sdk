91e69dacda5a4eb3eeefebfa044bd78e90aa9c05
jinjun <757258777@qq.com>
2023-07-24 18:21:47 +0800

    加入订单统计类接口

--- src/Statistics/Client.php
+++ src/Statistics/Client.php
@@ -53,4 +53,77 @@ class Client extends BaseApi
     {
         return $this->httpGet('/statistics/refund/amount', $query);
     }
+
+    /**
+     * 订单优惠金额统计
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function discountFee(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/discount', $query);
+    }
+
+    /**
+     * 订单平均客单价
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function customerFee(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/customer-amount', $query);
+    }
+
+    /**
+     * 商品销售数量top
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function productCountTop(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/product/count-top', $query);
+    }
+
+    /**
+     * 商品销售金额top
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function productAmountTop(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/product/amount-top', $query);
+    }
+
+    /**
+     * 商品销售金额top
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function totalFeeGroupByDate(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/amount/date', $query);
+    }
+
+    /**
+     * 商品销售金额top
+     *
+     * @param array $query
+     * @return array|\Psr\Http\Message\ResponseInterface|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function countGroupByDate(array $query = [])
+    {
+        return $this->httpGet('/statistics/order/count/date', $query);
+    }
+
 }
